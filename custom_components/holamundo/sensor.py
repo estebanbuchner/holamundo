792a2c33bdd4e73c866e9ef9c1541a667cd83b9e
Esteban Buchner <esteban.buchner@gmail.com>
2025-08-10 12:49:16 -0300

    Agrega sensor de hora actual

--- custom_components/holamundo/sensor.py
+++ custom_components/holamundo/sensor.py
@@ -1,7 +1,11 @@
 from homeassistant.helpers.entity import Entity
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    async_add_entities([HolaMundoSensor()])
+    async_add_entities([
+        HolaMundoSensor(),
+        HoraActualSensor()
+    ], True)
+
 
 class HolaMundoSensor(Entity):
     def __init__(self):
@@ -14,3 +18,20 @@ class HolaMundoSensor(Entity):
     @property
     def state(self):
         return self._state
+
+from datetime import datetime
+
+class HoraActualSensor(Entity):
+    def __init__(self):
+        self._state = None
+
+    @property
+    def name(self):
+        return "Hora Actual"
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        self._state = datetime.now().strftime("%H:%M:%S")
